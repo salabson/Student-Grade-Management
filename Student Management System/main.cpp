7741258eda64b467f8b57ac1ea91f3de9aff2a2f
Salabson <salabson4@yahoo.co.uk>
2019-05-29 12:38:49 +0100

    Refactor median and final grade calculations to their respective function

--- Student Management System/main.cpp
+++ Student Management System/main.cpp
@@ -4,10 +4,37 @@
 #include<iomanip>
 #include<vector>
 #include<algorithm>
-
-
+#include<stdexcept>
 
 using namespace std;
+
+double compute_final_grade(double midterm, double final_grade, double median ){
+    return 0.2*midterm + 0.4*final_grade + 0.4*median;
+}
+
+double compute_median(vector<double> vec){
+    // check that student enter some homework grade
+   typedef vector<double>::size_type vec_size;
+    vec_size size = vec.size();
+   if(size == 0){
+    throw domain_error("Median of the vector is empty");
+  }
+
+
+
+    // sort the grades
+  sort(vec.begin(), vec.end());
+
+  // compute median of homework grades
+  vec_size mid = size/2;
+
+  return size%2==0 ? vec[mid]+vec[mid-1]/2:vec[mid];
+
+
+}
+
+
+
 
 int main()
 {
@@ -17,15 +44,14 @@ int main()
     cin>>name;
     cout << "Hello " << name << "!" << endl;
 
-    // Read student midtern and final grade
+    // Read student midterm and final grade
     cout << "Enter your Midterm and Final Grades" << endl;
     double midterm_grade, final_grade;
     cin >> midterm_grade >> final_grade;
 
   // ask for Homework grades
   cout << "Enter all your homework grades" << endl;
-  int icount = 0;
-  double sum = 0;
+
   double homework_grade;
   vector<double> homework_grades;
 
@@ -33,25 +59,14 @@ int main()
   while(cin >> homework_grade){
    homework_grades.push_back(homework_grade);
    }
-   // check that student enter some homework grade
-   typedef vector<double>::size_type vec_size;
-   vec_size size = homework_grades.size();
-   if(size == 0){
-    cout << "You must enter at least one grade" << endl;
-    return 1;
-  }
 
-  // sort the grades
-  sort(homework_grades.begin(), homework_grades.end());
 
-  // compute median of homework grades
-  vec_size mid = size/2;
-  double median;
-  median = size%2==0 ? homework_grades[mid]+homework_grades[mid-1]/2:homework_grades[mid];
 
-  streamsize prec = cout.precision();
+   double median = compute_median(homework_grades);
+  // compute and write final grade
+   streamsize prec = cout.precision();
    cout << "Your final grade is " << setprecision(3)
-        << 0.2*midterm_grade + 0.4*final_grade + 0.4 *median
+        << compute_final_grade(midterm_grade,final_grade,median)
          << setprecision(prec) << endl;
     return 0;
 }
